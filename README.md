b92fad2d61fcc62c68c34bb038217c2a56cf7a8e
akshat <akshatbhargavadeepa@gmail.com>
2018-12-08 12:15:10 +0530

    basic template setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Project Starter
+
+Bundler: [<b>_ParcelJS_</b>](https://parceljs.org)
+
+
+Transpiler: [<b>_Babel_</b>](https://babeljs.io)
+
+_and node-sass package for stylesheets._
+
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import './main.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Akshat 5</h1>
+    </div>
+  )
+};
+
+render(<App />, document.getElementById("app"));
