defa1e95ffeb9898915d7e14417f0b48a87c69a0
sakshi srivastava <sakshisrivastava413@gmail.com>
2018-12-26 18:58:46 +0530

    added react-google-login plugin

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import jwt_decode from 'jwt-decode';
 import { Card, Input, Icon, Row, Button, Col, Tooltip } from 'antd';
-import GoogleLogin from './GoogleLogin';
+import { GoogleLogin } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import { API_BASE_URL, GOOGLE_APP_ID, FACEBOOK_APP_ID } from '../App.constants';
 
@@ -30,16 +30,24 @@ class EditProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: {
+        accountLinked: false,
+        email: '',
+        name: '',
+        phone: '',
+        profilePicture: '',
+        secondaryEmail: ''
+      }
     };
     this.handleChange = this.handleChange.bind(this);
     this.updateProfile = this.updateProfile.bind(this);
+    this.onSocialLink = this.onSocialLink.bind(this);
   }
 
   componentDidMount() {
     const token = localStorage.getItem('vc_token');
     if (token) {
-      this.setState({ user: jwt_decode(token) });
+      this.setState({ user: jwt_decode(token) }, () => console.log(this.state.user));
     }
   }
 
@@ -49,6 +57,48 @@ class EditProfile extends React.Component {
     }, () => {
       console.log(this.state)
     })
+  }
+
+  onSocialLink(data, accountType) {
+    let userData = {};
+    if (accountType == 'google') {
+      userData = {
+        token: data.accessToken,
+        email: data.profileObj.email,
+        name: data.profileObj.name,
+        imageUrl: data.profileObj.imageUrl
+      };
+    } else {
+      userData = {
+        token: data.accessToken,
+        email: data.email,
+        name: data.name,
+        imageUrl: data.picture.data.url
+      };
+    }
+    fetch(
+      API_BASE_URL + '/users/profile/link-social', {
+        method: 'PUT',
+        body: JSON.stringify({
+          ...userData,
+          accountType
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('vc_token')
+        }
+      }
+    ).then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        const { token, updatedUser } = res;
+        localStorage.setItem('vc_token', token);
+        this.setState({
+          user: updatedUser
+        });
+      }).catch((err) => {
+        alert(err.message);
+      });
   }
 
   updateProfile(e) {
@@ -79,11 +129,11 @@ class EditProfile extends React.Component {
         <br />
 
         <FormItem label="Name" tooltipTitle="Auto filled from your linked social media account">
-          <Input value="Sakshi Srivastava" disabled />
+          <Input value={this.state.user.name} disabled />
         </FormItem>
 
         <FormItem label="Email" tooltipTitle="Auto filled from your linked social media account">
-          <Input value="sakshisrivastava413@gmail.com" disabled />
+          <Input value={this.state.user.email} disabled />
         </FormItem>
 
         <FormItem label="Secondary Email" tooltipTitle="You'll receive order details here">
@@ -93,7 +143,7 @@ class EditProfile extends React.Component {
         <FormItem label="Mobile">
           <Row gutter={40}>
             <Col span={20}>
-              <Input placeholder="Enter your mobile number" value="9968516771" type="tel" onChange={r => console.log(r)} />
+              <Input placeholder="Enter your mobile number" value={this.state.user.phone} type="tel" onChange={r => console.log(r)} />
             </Col>
             <Col span={4}>
               <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" style={{ fontSize: 'x-large' }} />
@@ -109,10 +159,14 @@ class EditProfile extends React.Component {
           <Row gutter={50}>
             <Col span={12}>
               <GoogleLogin
-                onSuccess={(r) => console.log(r)}
                 clientId={GOOGLE_APP_ID}
                 onSuccess={(d) => this.onSocialLink(d, 'google')}
                 onFailure={(d) => console.log(d)}
+                render={renderProps => (
+                  <Button type="danger" onClick={renderProps.onClick} block style={{ background: '#DD4B39', color: 'white' }} icon="google-plus" disabled={this.state.user.accountLinked == 'google'}>
+                    {this.state.user.accountLinked == 'google' ? 'Account Linked' : 'Google+ Login'}
+                  </Button>
+                )}
               />
             </Col>
             <Col span={12}>
@@ -122,7 +176,9 @@ class EditProfile extends React.Component {
                 fields="name,email,picture"
                 callback={(d) => this.onSocialLink(d, 'facebook')}
                 render={renderProps => (
-                  <Button type="primary" onClick={renderProps.onClick} block style={{ background: '#3b5998', color: 'white' }} icon="facebook">Facebook Login</Button>
+                  <Button type="primary" onClick={renderProps.onClick} block style={styles.facebookButtonStyle} icon="facebook" disabled={this.state.user.accountLinked == 'facebook'}>
+                    {this.state.user.accountLinked == 'facebook' ? 'Account Linked' : 'Facebook Login'}
+                  </Button>
                 )}
               />
             </Col>
@@ -135,3 +191,8 @@ class EditProfile extends React.Component {
 };
 
 export default EditProfile;
+
+const styles = {
+  facebookButtonStyle: { background: '#3b5998', color: 'white' }
+
+}
