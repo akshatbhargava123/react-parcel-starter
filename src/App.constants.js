66146b85e762dd05c4422249460eb1965c17d941
akshat <akshatbhargavadeepa@gmail.com>
2018-12-30 22:10:50 +0530

    added firebase config

--- src/App.constants.js
+++ src/App.constants.js
@@ -5,3 +5,12 @@ export const FACEBOOK_APP_ID = '2141553712838298';
 export const GOOGLE_APP_ID = '84820326176-q0vquk02bsk8rcqf0v0hr5vstiv21rqi.apps.googleusercontent.com';
 
 export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50TGlua2VkIjoibm9uZSIsInBhc3N3b3JkU2V0IjpmYWxzZSwiX2lkIjoiNWMxYTdkYmIyYzA3MWI3OWQzYjdjYzRlIiwidXNlcm5hbWUiOiJDRDBMUE1NRF5ZRlJAIUtFVUciLCJwaG9uZSI6Ijk5Njg1MTY3NzEiLCJyb2xlIjoiY3VzdG9tZXIiLCJlbWFpbCI6Im51bGx2YWx1ZW1hZ2ljUEhTRSIsImxhc3RPdHBSZXF1ZXN0ZWQiOm51bGwsIm90cCI6bnVsbCwiaWF0IjoxNTQ1ODA0ODA3LCJleHAiOjE1NDU4OTEyMDd9.ERnfHEkia98-A3HOT2UHmdDLEmqqWNHU5Z2kh6CXcZU'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBKQV1spUpYsXzPnKPw7qnFOCPbtejN5Mw",
+  authDomain: "vc-admin-panel.firebaseapp.com",
+  databaseURL: "https://vc-admin-panel.firebaseio.com",
+  projectId: "vc-admin-panel",
+  storageBucket: "vc-admin-panel.appspot.com",
+  messagingSenderId: "772488433142"
+};
