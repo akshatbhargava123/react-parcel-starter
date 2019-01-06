74c9d1b876fb82b21a2f6859ca3cf7458234dc8b
akshat <akshatbhargavadeepa@gmail.com>
2019-01-06 09:41:56 +0530

    added product detail page route

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import AccountPage from './pages/AccountPage';
+import ProductDetails from './pages/ProductDetails';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/' exact component={HomePage} />
       <Route path='/account' component={AccountPage} />
+      <Route path='/product/:id' component={ProductDetails} />
     </Switch>
   </BrowserRouter>
 );
